34ebc75bc9b03348f22f8d18e71f71fd1db50081
Ross Chapman <ross.chapman@me.com>
2012-09-10 13:56:47 -0400

    Fixed some conflicts with the new version of Xcode

--- src/Three20UI/Sources/UIViewAdditions.m
+++ src/Three20UI/Sources/UIViewAdditions.m
@@ -83,15 +83,7 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 @interface UITouch (TTCategory)
 
-/**
- *
- */
-- (id)initInView:(UIView *)view location:(CGPoint)location;
 
-/**
- *
- */
-- (void)changeToPhase:(UITouchPhase)phase;
 
 @end
 
@@ -102,31 +94,6 @@
 @implementation UITouch (TTCategory)
 
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-- (id)initInView:(UIView *)view location:(CGPoint)location {
-	self = [super init];
-  if (self) {
-    _tapCount = 1;
-    _locationInWindow = location;
-    _previousLocationInWindow = location;
-
-    UIView *target = [view.window hitTest:_locationInWindow withEvent:nil];
-    _view = [target retain];
-    _window = [view.window retain];
-    _phase = UITouchPhaseBegan;
-    _touchFlags._firstTouchForView = 1;
-    _touchFlags._isTap = 1;
-    _timestamp = [NSDate timeIntervalSinceReferenceDate];
-  }
-  return self;
-}
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-- (void)changeToPhase:(UITouchPhase)phase {
-  _phase = phase;
-  _timestamp = [NSDate timeIntervalSinceReferenceDate];
-}
 
 
 @end
@@ -427,15 +394,7 @@ TT_FIX_CATEGORY_BUG(UIViewAdditions)
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (void)simulateTapAtPoint:(CGPoint)location {
-  UITouch *touch = [[[UITouch alloc] initInView:self location:location] autorelease];
-
-  UIEvent *eventDown = [[[UIEvent alloc] initWithTouch:touch] autorelease];
-  [touch.view touchesBegan:[NSSet setWithObject:touch] withEvent:eventDown];
-
-  [touch changeToPhase:UITouchPhaseEnded];
-
-  UIEvent *eventUp = [[[UIEvent alloc] initWithTouch:touch] autorelease];
-  [touch.view touchesEnded:[NSSet setWithObject:touch] withEvent:eventUp];
+	
 }
 
 #endif
